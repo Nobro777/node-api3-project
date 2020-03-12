816b865a72f53eb5f4f62eb870a1b00d7c9c77d5
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-03-12 08:59:00 -0700

    mvp stuff working

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code away!
+const express = require("express");
+const server = express();
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+
+const port = 5000;
+
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+server.listen(port, () => {
+    console.log(`\n* Server listening on http://localhost:${port} *\n`);
+});
